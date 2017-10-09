d78e6c7cdeeb55d9c8992e3fe890e889dc04c34d
Aaron Freeland <afreeland@gmail.com>
2017-10-09 14:05:39 -0700

    more changes

--- client.js
+++ client.js
@@ -1,6 +1,21 @@
-module.exports = function(app){
+module.exports = function(app, mongodbConn){
+
+    var MongoClient = require('mongodb').MongoClient;
+    // Connection url
+    var url = 'mongodb://' + mongodbConn.user + ':' + mongodbConn.pass + '@' + mongodbConn.host + ':' + mongodbConn.port + '/' + mongodbConn.db;
+    
+
     app.post('/register', (req, res) => {
-        console.log()
+        // Connect using MongoClient
+        MongoClient.connect(url, function(err, db) {
+            
+            var users = db.collection('users');
+            let _body = req.body;
+            // Check for summoner, insert if not present or update if it is
+            users.update({summoner: _body.summoner}, _body, { upsert: true})
+
+            res.send(_body);
+        });
     })
 
     app.get('/register', (req, res) => {
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
 
 let mongodbConn = {
   host: 'ds113775.mlab.com',
@@ -10,11 +11,33 @@ let mongodbConn = {
 }
 
 
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+
+// ## CORS middleware
+// 
+// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-require('./client.js')(app);
+require('./client.js')(app, mongodbConn);
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
